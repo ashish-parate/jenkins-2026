9113c114f85273351aefafb1d62ffe37774f9b6c
Ashish <ashish.parate3@gmail.com>
2026-01-29 14:07:23 +0530

    maven bulid

---
+++ student-app.groovy
@@ -0,0 +1,39 @@
+pipeline {
+    agent any
+
+    stages {
+          stage('pull') {
+            steps {
+                git 'https://github.com/javaparser/javaparser-maven-sample.git'
+                // Add build steps here
+            }
+        }
+        stage('Build') {
+            steps {
+                sh 'mvn clean package'
+                // Add build steps here
+            }
+        }
+        stage('Test') {
+            steps {
+                echo 'Testing successfully...'
+                // Add test steps here
+            }
+        }
+        stage('Deploy') {
+            steps {
+                echo 'Deploying successfully...'
+                // Add deploy steps here
+            }
+        }
+    }
+
+    post {
+        success {
+            echo 'Pipeline completed successfully!'
+        }
+        failure {
+            echo 'Pipeline failed.'
+        }
+    }
+}
